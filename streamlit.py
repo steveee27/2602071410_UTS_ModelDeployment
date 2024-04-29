8bd4b551289168f6a2f9c9df5b89bab1658d5983
Steve Marcello Liem <119674324+steveee27@users.noreply.github.com>
2024-04-29 18:16:33 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -5,7 +5,6 @@ import pandas as pd
 
 # Load the machine learning model and encoders
 binary_encode = joblib.load('Binary_Encode.pkl')
-onehot_encode = joblib.load('OneHot_Encoder.pkl')
 scaler = joblib.load('StandardScaler.pkl')
 model = joblib.load('xgb_classifier_model.pkl')
 
@@ -14,7 +13,6 @@ def main():
 
     # User input components
     creditscore = st.number_input("Credit Score", min_value=0)
-    geography = st.selectbox("Geography", ['Germany', 'Spain', 'France'])
     gender = st.selectbox("Gender", ['Male', 'Female'])
     age = st.number_input("Age", min_value=0)
     tenure = st.number_input("Tenure", min_value=0)
@@ -26,7 +24,6 @@ def main():
 
     # Convert categorical inputs to encoded form
     gender_encoded = binary_encode['Gender'][gender]
-    geography_encoded = onehot_encode.transform(np.array(geography).reshape(1, -1)).toarray().reshape(-1)
     hascrcard_encoded = binary_encode['HasCrCard'][hascrcard]
     isactivemember_encoded = binary_encode['IsActiveMember'][isactivemember]
 
@@ -37,6 +34,8 @@ def main():
         'Tenure': tenure,
         'Balance': balance,
         'NumOfProducts': numofproducts,
+        'HasCrCard': hascrcard_encoded,
+        'IsActiveMember': isactivemember_encoded,
         'EstimatedSalary': estimatedsalary
     }
     input_df = pd.DataFrame([input_data])
@@ -44,17 +43,13 @@ def main():
     # Scale numerical inputs
     scaled_inputs = scaler.transform(input_df)
 
-    # Concatenate encoded and scaled inputs
-    final_input = np.concatenate([scaled_inputs, geography_encoded.reshape(1, -1)], axis=1)
-
     if st.button('Predict'):
-        prediction = make_prediction(final_input)
+        prediction = make_prediction(scaled_inputs)
         st.success(f'The prediction is: {prediction}')
 
 def make_prediction(features):
     # Use the loaded model to make predictions
-    input_array = np.array(features).reshape(1, -1)
-    prediction = model.predict(input_array)
+    prediction = model.predict(features)
     return prediction[0]
 
 if __name__ == '__main__':
